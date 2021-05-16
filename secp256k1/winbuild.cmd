944e904ad4e889efcade8096e591734417b7ef12
acceleratorlastorder <mr.hamidi.okba@gmail.com>
2021-05-16 02:01:34 +0200

    add build target folder in .cmd sample, add reference to vcvars64.bat for easy copy paste remove miner.exe, indent autolykos.cu

--- secp256k1/winbuild.cmd
+++ secp256k1/winbuild.cmd
@@ -1,22 +1,25 @@
-SET "OPENSSL_DIR=C:\openssl102"
-SET "LIBCURL_DIR=C:\Users\mam0nt\curl"
+SET "OPENSSL_DIR=C:\OpenSSL-Win64"
+SET "LIBCURL_DIR=REPLACE_WITH_LIBCURL_FOLDER"
 SET "CUDA_COMPUTE_ARCH=61"
 SET "BLOCK_DIM=64"
 SET "WORKSPACE=0x800000"
 cd src
  
-nvcc  -use_fast_math -maxrregcount=128 -o ../miner.exe -Xcompiler "/std:c++14" -gencode=arch=compute_%CUDA_COMPUTE_ARCH%,code=\"sm_%CUDA_COMPUTE_ARCH%,compute_%CUDA_COMPUTE_ARCH%\"^
- -DBLOCK_DIM=%BLOCK_DIM% -DNONCES_PER_ITER=%WORKSPACE%^
- -I %OPENSSL_DIR%\include ^
- -I %LIBCURL_DIR%\include ^
- -l %LIBCURL_DIR%\lib\libcurl ^
- -l %OPENSSL_DIR%\lib\libeay32 -L %OPENSSL_DIR%/lib ^
- -lnvml ^
-cpuAutolykos.cc conversion.cc cryptography.cc definitions.cc jsmn.c httpapi.cc ^
-mining.cu prehash.cu processing.cc request.cc easylogging++.cc autolykos.cu httplib.cc
+::"c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+ 
+nvcc -use_fast_math -maxrregcount=128 -o ../builds/miner.exe -Xcompiler "/std:c++14" ^
+    -gencode=arch=compute_%CUDA_COMPUTE_ARCH%,code=\"sm_%CUDA_COMPUTE_ARCH%,compute_%CUDA_COMPUTE_ARCH%\"^
+    -DBLOCK_DIM=%BLOCK_DIM% -DNONCES_PER_ITER=%WORKSPACE%^
+    -I %OPENSSL_DIR%\include ^
+    -I %LIBCURL_DIR%\include ^
+    -l %LIBCURL_DIR%\lib\libcurl ^
+    -l %OPENSSL_DIR%\lib\libeay32 -L %OPENSSL_DIR%/lib ^
+    -lnvml ^
+    cpuAutolykos.cc conversion.cc cryptography.cc definitions.cc jsmn.c httpapi.cc ^
+    mining.cu prehash.cu processing.cc request.cc easylogging++.cc autolykos.cu httplib.cc
 
 cd ..
-SET PATH=%PATH%;C:\Program Files\NVIDIA Corporation\NVSMI
+::SET PATH=%PATH%;C:\Program Files\NVIDIA Corporation\NVSMI
 
 
 
